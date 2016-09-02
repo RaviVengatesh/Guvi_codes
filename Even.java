53ef0b15ec5a42319e1bb8a930c589b59f98bc9b
RaviVengatesh <ravivengat1996@gmail.com>
2016-09-02 14:19:14 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,16 @@
+public class Even {
+ 
+        public static void main(String[] args) {
+               
+                int li = 50;
+               
+                System.out.println("Printing Odd numbers between 1 and "+li);
+               
+                for(int i=1; i <= li; i++){
+                       
+                        if( i % 2 = 0){
+                                System.out.print(i + " ");
+                        }
+                }
+        }
+}
