7d56f43deb2a139b89c0c400033e8cc629abb6c7
RaviVengatesh <ravivengat1996@gmail.com>
2016-09-02 14:40:52 +0530

    Create fact12.java

---
+++ fact12.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class fact12 {
+
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int f1=1;
+        for(i=1;i<=n;i++)
+        {
+          f1=f1*i;
+        }
+         System.out.println(f1);
+        
+    }
