e10bc46356033d8fabbcf741cdfd61bd44426daf
RaviVengatesh <ravivengat1996@gmail.com>
2016-09-02 11:13:46 +0530

    Create apha1.c

---
+++ apha1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  char ip;
+printf("\n enter the alphabet");
+  scanf("%c",&ip);
+  if(ip==65&&90||97&&122)
+    printf("\n the given thing is alphabet");
+ else
+    printf("\n the given thing is not alphabet");
+	return 0;
+}
